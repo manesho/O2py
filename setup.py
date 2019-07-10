d5ad18bfe7b0fc111845632500c7189244622614
manes <manes.hornung@gmail.com>
2019-07-10 16:24:59 +0200

    working standalone package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='O2py',
+      version='0.1',
+      description='An interactive visualization of the 2d O(2) model with matplotlib',
+      author='Manes Hornung',
+      author_email='hornung@itp.unibe.ch',
+      #license='MIT',
+      packages=['O2py'],
+      install_requires=['numpy', 'scipy', 'matplotlib', 'numba'],
+      zip_safe=False)
